80b00f4a2c8edeec5b4bd956f663129a966a0de2
rycue <151157051+rycue@users.noreply.github.com>
2025-03-10 09:04:10 +0800

    Add DecToBin w/ steps visualizer

--- CPP/NumSysConverter.cpp
+++ CPP/NumSysConverter.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
-int decimalNumber, binaryNumber, octalNumber;
-
 int main() {
-    int inputNumber;
-    cout << "Enter a number: ";
-    cin >> inputNumber;
+    // DECLARATION
+    int decimalNumber, tempNumber;
+    string binaryNumber;
 
-    decimalNumber = inputNumber;
+    // USER INPUT INITIALIZATION
+    cout << "Enter a decimal number: ";
+    cin >> decimalNumber;
 
-    cout << "\nDecimal: " << decimalNumber << endl;
+    // DISPLAY USER
+    cout << "\nDecimal Number: " << decimalNumber << "\n" << endl;
 
     // DECIMAL TO BINARY
     if (decimalNumber == 0) {
-        binaryNumber = 0;
-    } else {
-        while (decimalNumber > 0) {
-            binaryNumber = binaryNumber * 10 + decimalNumber % 2;
-            decimalNumber /= 2;
+        binaryNumber = "0";
+    }
+    while (decimalNumber > 0) {
+        cout << "(before) binary in-process: " << binaryNumber << endl;
+        cout << decimalNumber << " % 2 = " << decimalNumber % 2 << endl;
+        binaryNumber = to_string(decimalNumber % 2) + binaryNumber;
+        cout << "(after) binary in-process: " << binaryNumber << endl;
+        cout << decimalNumber << " / 2 = " << decimalNumber / 2;
+        if (decimalNumber / 2 == 0)
+            cout << " (end of loop)\n" << endl;
+        else {
+            cout << endl << "\n--- --- another loop --- ---" << endl;
         }
+        decimalNumber /= 2;
+    }
+
+    while (binaryNumber.length() < 4) {
+        binaryNumber = "0" + binaryNumber;
     }
 
     cout << "Binary: " << binaryNumber << endl;
-
-    // DECIMAL TO OCTAL
 }
