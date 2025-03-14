f4e809c1e737c393212f4a8f424da9873ad51d59
Rycue <151157051+rycue@users.noreply.github.com>
2025-03-14 11:38:36 +0800

    Fix CPP file

--- CPP/CPP-ACT3-Q10-4.cpp
+++ CPP/CPP-ACT3-Q10-4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     // VARIABLE DECLARATION AND INITIALIZATION
     double numOne = 0.0, numTwo = 0.0;
-    double sum = 0.0;
+    double sum = 0.0, product = 0.0;
 
     // USER INPUT - Get the two numbers
     cout << "Enter a number: ";
@@ -13,12 +13,15 @@ int main() {
     cout << "Enter another number: ";
     cin >> numTwo;
 
-    // PROCESS - Adding two numbers to get the sum
-    sum = numOne + numTwo;
+    // PROCESS
+    sum = numOne + numTwo; // get the sum
+    product = numOne * numTwo; // get the product
 
     // DISPLAY ON CONSOLE - result of calculation
     cout << "Sum: " << sum << " = " << numOne << " + " << numTwo << endl;
     cout << "Therefore, the sum is " << sum << endl;
+    cout << "\nProduct: " << product << " = " << numOne << " x " << numTwo << endl;
+    cout << "Therefore, the product is " << product << endl;
 
     return 0;
 }
