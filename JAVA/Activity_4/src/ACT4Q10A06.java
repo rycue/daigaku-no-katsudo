21d463d7603c954512a204869244a5a0f35fd66e
rycue <151157051+rycue@users.noreply.github.com>
2025-09-24 22:35:38 +0800

    Initialize ACT4 java files

---
+++ JAVA/Activity_4/src/ACT4Q10A06.java
@@ -0,0 +1,21 @@
+public class ACT4Q10A06 {
+    public static void main(String[] args) {
+        
+        // INSTANCES
+        
+        // VARIABLES
+
+        // HEADER
+        System.out.println("=============================");
+        System.out.println("=   =");
+        System.out.println("=============================\n");
+        
+        // INPUT & OUTPUT
+
+        // FOOTER
+        System.out.println("\n=============================");
+        System.out.println("PROGRAMMER: CUENZA, RICKY S.");
+        System.out.println("=============================");
+
+    }
+}
