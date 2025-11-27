dde0d46f83398c2afbb6e431ca052194d4e0b3e8
rycue <151157051+rycue@users.noreply.github.com>
2025-11-27 13:22:14 +0800

    feat: create 'JAVA-251124-001 .java' and Day03_RickyCuenza.java

---
+++ JAVA/Others/src/JAVA-251124-001 .java
@@ -0,0 +1,7 @@
+package Others.src;
+
+public class JAVA-251124-001 {
+    public static void main(String[] args) {
+        System.out.println("HEllo");
+    }
+}
---
+++ JAVA/Quiz_2/src/Day03_RickyCuenza.java
@@ -0,0 +1,31 @@
+// Author: Ricky Cuenza (Batch 3 JOYSIS TVI)
+// Date Created: November 19, 2025
+
+public class Day03_RickyCuenza {
+    public static void main(String[] args) {
+
+        int num1 = 11;
+        int num2 = 22;
+        double num3 = 33.33;
+        double num4 = 44.44;
+        int result1 = 0;
+        double result2 = 0;
+
+        result1 = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result1);
+
+        result1 = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + result1);
+
+        result1 = num1 % num2;
+        System.out.println("The remainder of " + num1 + " % " + num2 + " is " + result1);
+                
+        result2 = num3 * num4;
+        System.out.println(num3 + " * " + num4 + " = " + result2);
+
+        result2 = num3 / num4;
+        System.out.println(num3 + " / " + num4 + " = " + result2);
+
+        System.out.println("\nProgram by Ricky Cuenza (JOYSIS TVI - Batch 3)");
+    }
+}
